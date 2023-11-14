ed081cd79a3847ee00903346a6e12ec708bfb4f4
Jeronimo-Tzib <144931484+Jeronimo-Tzib@users.noreply.github.com>
2023-11-14 03:16:29 -0600

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const heart = document.querySelector('.heart');
-    const heartContainer = document.querySelector('.heart-container');
+    
+    function createStars() {
+        const container = document.querySelector('.stars-container');
+        for (let i = 0; i < 1000; i++) { // Adjust the number of stars as needed
+            const star = document.createElement('div');
+            star.classList.add('star');
+            star.style.left = `${Math.random() * 100}vw`;
+            star.style.top = `${Math.random() * 100}vh`;
+            container.appendChild(star);
+        }
+    }
 
-    // Add pulsating animation to the heart
-    heart.style.animation = 'pulse 1.5s infinite';
-
-    // Add neon border animation to the heart container
-    heartContainer.style.boxShadow = '0 0 10px #ff65a3';
-    setInterval(() => {
-        heartContainer.style.boxShadow = '0 0 10px #ff65a3';
-        setTimeout(() => {
-            heartContainer.style.boxShadow = '0 0 20px #ff65a3';
-        }, 500); // Adjust the duration as needed
-    }, 2000); // Adjust the interval as needed
+    
+    createStars();
 });
